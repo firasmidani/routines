893a9bd448a9d0b37dd8d8c43076d016e5263673
Firas Midani <firas.midani@duke.edu>
2015-11-23 18:18:40 -0500

    modified slurm_job_info.py

--- slurm_job_info.py
+++ slurm_job_info.py
@@ -3,6 +3,7 @@
 #title         :slurm_job_info.py 
 #description   :checks attributes of cluster job using slurm scontrol
 #author        :Firas Said Midani
+#3-mai         :firas.midani@duke.edu
 #date          :2015.11.23
 #version       :1.0
 #usage         :python slurm_job_info.py [job id] [job attribute] 
@@ -10,32 +11,33 @@
 # given a job_array_main_id 
 # grab information about all jobs in the array from slurm
 
-##arguments
+##ARGUMENTS
 # job id
 # desired attribute (any attribute that "scontrol show job" would return about a job)
 
 ##OUTPUT
 # information of desired attribute
 
-import pandas as pd, \
-       sys, \
-       os
+import \
+pandas as pd, \
+subprocess,   \
+sys, \
+os
 
-import subprocess
-
+## process arguments
 job_array_main_id = sys.argv[1]
 job_arg_out       = sys.argv[2]
 
+## necessary for importing shell output into python 
 proc         = subprocess.Popen(["scontrol","show","job",job_array_main_id],stdout=subprocess.PIPE,shell=False)
 (output,err) = proc.communicate()
-
-output = output.split('\n\n')[:-1];
+output       = output.split('\n\n')[:-1];
 
 #for each job, create a dictionary with job features and their settings
 job_info={}
 for job in range(len(output)):
-	single_job = " ".join(output[job].split('\n   ')).split(' ')
 	job_dict={}
+	single_job = " ".join(output[job].split('\n   ')).split(' ')
 	for value in single_job:
 		job_dict[value.split('=')[0]]=value.split('=')[1]
 	job_info[job_dict['JobId']]=job_dict;
